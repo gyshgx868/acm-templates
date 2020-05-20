85b2cb1090cbff3a684121191f4cc38404af1536
gyshgx868 <1187608884@qq.com>
2020-05-20 11:17:24 +0800

    Optimize quick_pow.

--- math/number_theory/quick_pow.cpp
+++ math/number_theory/quick_pow.cpp
@@ -3,8 +3,8 @@ T quick_pow(T x, U n, T m) {
   if (n == U() || x == 1) { return 1 % m; }
   T res = 1;
   while (n > U()) {
-    if (n & 1) { res = res * x % m; }
-    x = x * x % m;
+    if (n & 1) { res = static_cast<T>(1LL * res * x % m); }
+    x = static_cast<T>(1LL * x * x % m);
     n >>= 1;
   }
   return res % m;
